b2ada10d9843ba1fee8b125bdf4b9ebac7d159c7
jose v <jvazquez34139@gmail.com>
2018-11-16 17:53:03 -0600

    added rotation and other functions
    collision detections
    rotation
    minor reformating
    notes on slightly more advanced concepts

--- Examples/myConnectFour/js/app.js
+++ Examples/myConnectFour/js/app.js
@@ -1,23 +1,28 @@
-const game = new Game("theCanvas");
+const game = new CanvasGame("theCanvas");
 
 const uno = game.image(123,123,100,100,"res/flower.jpg");
 const bgDefault = game.rect(0,0,game.canvas.width,game.canvas.height,"black");
 const megaman = game.spriteAnimation(250,250,50,50,33,43,"res/spritesheet-demo.png",3);
+const sampleText = game.textString("heyyyyyyy","24px Tahoma",400,400);
+sampleText.color = "white";
 let tick = 0;
+console.log(sampleText.width);
 
 const update = () => {
-	uno.x += 1;
+	// uno.x += 1;
 	tick++;
-	if(tick % 10 == 0){
-		// megaman.nextFrame();
+	if(tick % 5 == 0){
+		megaman.nextFrame();
 	}
 }
 
 const render = () => {
 	update();
 	bgDefault.draw();
-	uno.draw();
 	megaman.draw();
+	sampleText.draw();
+	uno.rotate(tick);
+
 }
 
 game.run(render,60);
--- GameGraphicsNFunctions.js
+++ GameGraphicsNFunctions.js
@@ -1,7 +1,7 @@
 //jsgfl V2.0
 //required to run any canvas function
 //use canvas Id
-class Game{
+class CanvasGame{
 
 	constructor(canvas){
 		this.canvas = document.getElementById(canvas);
@@ -11,12 +11,34 @@ class Game{
 		setInterval(render, 1000/fps);
 	}
 
+	
 	//basic graphics/////////////////////////////////////////////////////
 	/*VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
 	//basic graphics/////////////////////////////////////////////////////
 	vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv*/
 	//basic graphics/////////////////////////////////////////////////////
-
+	textString(string, font, x, y){
+		const string_ = {
+			string: string,
+			font: font,
+			x: x,
+			y: y,
+			color: "black",
+			visible: true,
+			deg: 0,
+			draw: () => {
+				this.ctx.fillStyle = string_.color;
+				this.ctx.font = string_.font;
+				this.ctx.fillText(string_.string,string_.x,string_.y);
+			},
+			rotate: () => {
+				this.rotateFunction(string_);
+			}
+		};
+		string_.width = this.textSize(string_.string, string_.font).width;
+		string_.height = this.textSize(string_.string, string_.font).width;
+		return string_;
+	}
 	point(x,y){
 		const point_ = {
 			x: x,
@@ -47,13 +69,17 @@ class Game{
 			visible: true,
 			xDelta: line_.xf - line_.xi,
 			yDelta: line_.yf - line_.yi,
-			length: Math.sqrt(Math.pow(line_.xDelta,2) + Math.pow(line_.yDelta,2)),
+			length: Math.floor(Math.sqrt(Math.pow(line_.xDelta,2) + Math.pow(line_.yDelta,2))),
 			color: "black",
 			thickness: 1,
 			points: () => {
-				pointArr = [];
-				for(let i = 0; i < Math.floor(line_.length); i++){
-					pointArr[i] = this.point((line_.xi + (line_.xDelta / line_.length * i)),(line_.yi + (line_.yDelta / line_.length * i)))
+				const points_ = {
+					x: [],
+					y: []
+				};
+				for(let i = 0; i < line_.length; i++){
+					points_.x[i] = (line_.xi + (line_.xDelta / line_.length * i));
+					points_.y[i] = (line_.yi + (line_.yDelta / line_.length * i));
 				}
 				return pointArr;
 			},
@@ -75,6 +101,7 @@ class Game{
 			width: width,
 			height: height,
 			color: color,
+			deg: 0,
 			visible: true,
 			draw: () => {
 				this.ctx.fillStyle = rect_.color;
@@ -85,6 +112,9 @@ class Game{
 					rect_.height
 				);
 			},
+			rotate: () => {
+				this.rotateFunction(rect_)
+			}
 		}
 		return rect_;
 	}
@@ -127,6 +157,7 @@ class Game{
 			y: y,
 			width: width,
 			height: height,
+			deg: 0,
 			visible: true,
 			sprite: () => {
 				const imgSprite = new Image();
@@ -141,6 +172,9 @@ class Game{
 					image_.width,
 					image_.height
 				);
+			},
+			rotate: () => {
+				this.rotateFunction(image_);
 			}
 		}
 		return image_;
@@ -152,6 +186,7 @@ class Game{
 			y: y,
 			width: width,
 			height: height,
+			deg: 0,
 			visible: true,
 			totalFrames: totalFrames,
 			//where to start on sprite sheet
@@ -162,6 +197,7 @@ class Game{
 			//gap between sprite frames in sprite sheet given as src
 			frameNum: 0,
 			spriteGap: 1,
+			//assigns new spritesheet start
 			ssStart: (newX, newY) => {
 				animation_.ssStartX = newX;
 				animation_.ssStartY = newY;
@@ -194,13 +230,120 @@ class Game{
 					animation_.height
 				);
 			},
+			rotate: () => {
+				this.rotateFunction(animation_);
+			},
 			nextFrame: () => {
 				animation_.frameNum++;
 				if(animation_.frameNum >= totalFrames){
 					animation_.frameNum = 0;
 				}
 			}
+
 		}
 		return animation_;
+	}
+
+
+	//more than just drawing/////////////////////////////////////////////
+	/*VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+	//more than just drawing/////////////////////////////////////////////
+	vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv*/
+	//more than just drawing/////////////////////////////////////////////
+	rotateFunction(object){
+		//make it dry
+		let rad = object.deg * Math.PI / 180;
+		//gets center point of object
+		const cx = object.x + object.width / 2;
+		const cy = object.y + object.height / 2;
+
+		//holds the original x,y for reference
+		const tempX = object.x;
+		const tempY = object.y;
+
+		//pulls center of object to the top left of canvas
+		object.x = -object.width/2;
+		object.y = -object.height/2;
+
+		//save current canvas state to restore later
+		this.ctx.save()
+
+		//places center of rotation over center of object
+		this.ctx.translate(cx,cy);
+
+		this.ctx.rotate(rad);
+
+		//draw before restoring
+		object.draw(object);
+
+		//move canvas center back to normal and restore
+		this.ctx.translate(-cx,-cy);
+		this.ctx.restore();
+
+		//move object back to original location
+		if(object.x != tempX){
+			object.x = tempX;
+		}
+		if(object.y != tempY){
+			object.y = tempY;
+		}
+	}
+	//collision checking
+	collisionCheck(r1, r2){
+		//checks if any corners of rect1 are in or touching rect2
+		if(r1.x + r1.width >= r2.x && r1.x <= r2.x + r2.width &&
+	        r1.y <= r2.y + r2.height && r1.y + r1.height >= r2.y){
+	        return true;
+	    }
+	    else{
+	        return false;
+	    }
+	}
+	collisionUp(r1, r2){
+		if(r1.y <= r2.y + r2.height && r1.y > r2.y + r2.height / 4 * 3){
+            return true;
+        }else{
+            return false;
+        }
+	}
+	collisionDown(r1, r2){
+		if(r1.y + r1.height >= r2.y && r1.y + r1.height <= r2.y + r2.height / 4){
+            return true;
+        }else{
+            return false;
+        }
+	}
+	collisionLeft(r1, r2){
+		if(r1.x <= r2.x + r2.width && r1.x >= r2.x + r2.height / 4 * 3){
+            return true;
+        }else{
+            return false;
+        }
+	}
+	collisionRight(r1, r2){
+		if(r1.x + r1.width >= r2.x && r1.x + r1.width <= r2.x + r2.width / 4){
+            return true;
+        }else{
+            false;
+        }
+	}
+	collisionDiagnol(line, rect){
+		//will need to look for more efficient checking method
+		const pointChecker = this.point(line.points().x[0], line.points().y[0]);
+		for(var i = 0; i < line.length; i++){
+			pointChecker.x = line.points().x[i];
+			pointChecker.y = line.points().y[i];
+        	collisionCheck(pointChecker,rect);
+	    }
+	}
+	//miscellaneous 
+	textSize(string, font){
+		this.ctx.font = font;
+		this.ctx.measureText(string).width;
+		const size = {
+			width: this.ctx.measureText(string).width,
+			height: this.ctx.measureText(string).height
+		}
+		return size;
 	}
 }
